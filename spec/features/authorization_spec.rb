5c30bf77809a256637290ed4e2c84e264b8609fc
nakamura.hiroshi <nakamura.hiroshi@pocke.co.jp>
2020-02-12 12:14:27 +0900

    E2Eテストのバグ修正

--- spec/features/authorization_spec.rb
+++ spec/features/authorization_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
 
 RSpec.feature "Authorization", type: :feature do
-  scenario 'ログインする' do
-    # トップページを開く
-    visit "/users/sign_in"
+  scenario 'ユーザー登録してログアウト、ログインする' do
+    # 登録ページを開く
+    visit "/users/sign_up"
+    fill_in 'user_email', with: 'testtest@example.com'
+    fill_in 'user_password', with: '123456'
+    fill_in 'user_password_confirmation', with: '123456'
+    click_on '登録'
+    # 登録に成功したことを検証する
+    expect(page).to have_content 'アカウント登録が完了しました。'
+
+    # メインページに遷移するので、ログアウト
+    click_link 'ログアウト'
+    click_on 'ログイン'
+
     # ログインフォームにEmailとパスワードを入力する
-    fill_in 'user[email]', with: 'test1@example.com'
-    fill_in 'user[password]', with: '123456'
+    fill_in 'user_email', with: 'testtest@example.com'
+    fill_in 'user_password', with: '123456'
     # ログインボタンをクリックする
     click_on 'ログイン'
     # ログインに成功したことを検証する
